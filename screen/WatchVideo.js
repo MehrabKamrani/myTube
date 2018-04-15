3684a554fce37b55ed272d506f771eb3cbd8fbcc
Rahimi Diallo <tard916@outlook.com>
2018-04-16 07:50:29 +0800

    final commit

--- screen/WatchVideo.js
+++ screen/WatchVideo.js
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    height: 45,
+    height: 35,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
