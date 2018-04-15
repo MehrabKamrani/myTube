76facbf8ec0afb7fe7116057ec7378f740815cd8
Umar Adkhamov <35759152+UmarAdkham@users.noreply.github.com>
2018-04-15 20:54:04 +0800

    Change App.js
    Exlude ApproveVideo.js to access it from adminProfile.js

--- App.js
+++ App.js
@@ -8,13 +8,12 @@ import SignUP from './screen/signUp';
 import Activation from './screen/activation';
 import UserProfile from './screen/userProfile';
 import AdminProfile from './screen/adminProfile';
-import ApproveVideo from './screen/ApproveVideo';
 
 const AuthStack = StackNavigator({
   Login: {screen: Login},
   SignUP: {screen: SignUP},
   Activation: {screen: Activation},
-  ApproveVideo: {screen: ApproveVideo},
+
 });
 
 export default SwitchNavigator({
@@ -22,3 +21,4 @@ export default SwitchNavigator({
   UserProfile: {screen: UserProfile},
   AdminProfile: {screen: AdminProfile},
 });
+
