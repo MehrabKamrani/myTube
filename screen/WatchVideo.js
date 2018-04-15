6dc7fac02847eab2996a4704da8d337a8e3a82a2
Rahimi Diallo <tard916@outlook.com>
2018-04-15 14:57:14 +0800

    PK Level and Watch Video

--- screen/WatchVideo.js
+++ screen/WatchVideo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, TouchableHighlight, Text,Alert, TextInput} from 'react-native';
+import { StyleSheet,ScrollView, View, TouchableHighlight, Text,Alert, TextInput} from 'react-native';
 import VideoPlayer from '@expo/videoplayer';
 import { Ionicons } from '@expo/vector-icons';
 import BaseScreen from './BaseScreen';
@@ -85,30 +85,10 @@ export default class CustomScreen extends BaseScreen {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container}>
-        
-          <VideoPlayer
-            videoProps={{
-              shouldPlay: false,
-              resizeMode: Video.RESIZE_MODE_CONTAIN,
-              source: {
-                uri: `${this.state.Text_VideoPath}`,
-              },
-              isMuted: false,
-            }}
-            playIcon={icon('ios-play-outline')}
-            pauseIcon={icon('ios-pause-outline')}
-            fullscreenEnterIcon={icon('ios-expand-outline', 28)}
-            fullscreenExitIcon={icon('ios-contract-outline', 28)}
-            trackImage={require('../assets/track.png')}
-            thumbImage={require('../assets/thumb.png')}
-            textStyle={{
-              color: COLOR,
-              fontSize: 12,
-            }}
-            isPortrait={this.state.isPortrait}
-            switchToLandscape={this.switchToLandscape.bind(this)}
-            switchToPortrait={this.switchToPortrait.bind(this)}
-            playFromPositionMillis={0}
+          <Video source={{ uri: `http://www.224tech.com/reactPhp/videos/${this.state.Text_VideoPath}` }}
+            resizeMode="cover"
+            shouldPlay={true}
+            style={stylesss.photo}            
           />
           <TextInput placeholder="Video ID is sown here"
             
@@ -136,3 +116,13 @@ export default class CustomScreen extends BaseScreen {
     );
   }
 }
+
+const stylesss = StyleSheet.create({
+
+  photo: {
+    flex:1,
+    height: 200,
+    width: '100%',
+    borderRadius: 20,
+  },
+});
